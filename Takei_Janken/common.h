05275ef9440e90d4fc52538aa0bf40f30b47a48e
FujiFuji-K <vga2301239fujiwara@gmail.com>
2023-11-04 13:48:50 +0900

    Takei_Janken
    共通ファイル作成

---
+++ Takei_Janken/common.h
@@ -0,0 +1,20 @@
+//‹¤’Ê‚µ‚Äg—p‚·‚é—ñ‹“‚È‚Ç
+#pragma once
+
+
+namespace Common
+{
+	//è‚Ì—ñ‹“
+	enum eHand {
+		Rock = 0,
+		Scissors = 1,
+		Paper = 2,
+	};
+
+	//Ÿ•‰Œ‹‰Ê‚Ì—ñ‹“
+	enum eResult
+	{
+		PlayerWin = 0,
+		CPUWin = 1,
+	};
+}
